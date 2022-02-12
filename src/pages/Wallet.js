2e9c7036733f4b39c61a9f26272d92ef6ef47461
Victor Roesner <victorroesner@hotmail.com>
2022-02-12 19:09:44 -0300

    req 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from '../components/Expenses';
+import ExpenseTable from '../components/ExpenseTable';
 import Header from '../components/Header';
 
 class Wallet extends React.Component {
@@ -8,6 +9,7 @@ class Wallet extends React.Component {
       <>
         <Header />
         <Expenses />
+        <ExpenseTable />
 
       </>
 
